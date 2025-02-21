7eb9417c6ddf8bef1d4cfbec7e248501da53a67b
HoldenWustl <h.j.weinberg@wustl.edu>
2025-02-21 15:35:08 -0600

    Update server.js

--- server.js
+++ server.js
@@ -366,7 +366,7 @@ app.post('/webhooks', express.raw({ type: 'application/json' }), (req, res) => {
     console.log(`🎉 Payment completed for: ${session.customer_details.email}`);
     lastPayment = { email: session.customer_details.email };
     
-      io.emit("payment-success", lastPayment);
+      socket.emit("payment-success", lastPayment);
     
     
    
